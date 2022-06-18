d69380965bb762f7fb2c2fb5e8b24596516e488c
Andrew R <abstractdev0@gmail.com>
2022-06-18 12:44:08 -0400

    rm baseuser and extend user

--- src/entity/Author.ts
+++ src/entity/Author.ts
@@ -1,9 +1,9 @@
 import { Entity, Column, OneToMany } from "typeorm";
-import { BaseUser } from "./BaseUser";
+import { User } from "./User";
 import { Blogpost } from "./Blogpost";
 
 @Entity()
-export class Author extends BaseUser {
+export class Author extends User {
   @Column({ length: 20 })
   first_name: string;
 
--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,12 +1,27 @@
-import { Entity, OneToMany, Column } from "typeorm";
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  BaseEntity,
+  OneToMany,
+} from "typeorm";
 import { Comment } from "./Comment";
-import { BaseUser } from "./BaseUser";
+export type UserRole = "user" | "author" | "admin";
 
 @Entity()
-export class User extends BaseUser {
-  @Column({ default: false })
-  is_admin: boolean;
+export class User extends BaseEntity {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @Column({ length: 20, unique: true })
+  username: string;
+
+  @Column({ length: 200 })
+  password: string;
 
   @OneToMany(() => Comment, (comment) => comment.user)
   comments: Comment[];
+
+  @Column({ type: "enum", enum: ["user", "author", "admin"], default: "user" })
+  role: UserRole;
 }
