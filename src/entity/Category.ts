bf49b605bd6fe6fbf992c35085855d406d147f25
Andrew R <abstractdev0@gmail.com>
2022-07-01 09:46:16 -0400

    create category entity

---
+++ src/entity/Category.ts
@@ -0,0 +1,20 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  BaseEntity,
+  Column,
+  ManyToMany,
+} from "typeorm";
+import { Blogpost } from "./Blogpost";
+
+@Entity()
+export class Category extends BaseEntity {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @Column()
+  name: string;
+
+  @ManyToMany(() => Blogpost, (blogpost) => blogpost.categories)
+  blogposts: Blogpost[];
+}
