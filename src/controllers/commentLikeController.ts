99165f707140b1f143d75fc4100218988643fb4d
Andrew R <abstractdev0@gmail.com>
2022-07-01 09:44:39 -0400

    add commentLike Post, Del

---
+++ src/controllers/commentLikeController.ts
@@ -0,0 +1,79 @@
+import { Response, NextFunction } from "express";
+import { getRepository } from "typeorm";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+import { CommentLike } from "../entity/CommentLike";
+import { verifyToken } from "../auth/bearerAuthorization";
+dotenv.config();
+
+export const commentLikePost = [
+  verifyToken,
+  // Process any after validation and sanitization.
+  (req: any, res: Response, next: NextFunction) => {
+    // Extract validation errors and send if not empty.
+    jwt.verify(
+      req.token,
+      process.env.JWT_SECRET!,
+      (err: any, authData: any) => {
+        if (err) {
+          res.sendStatus(403);
+        } else {
+          const { is_liked } = req.body;
+          if (err) {
+            res.json(err);
+            return next(err);
+          } else {
+            (async () => {
+              //create commentLike instance
+              const commentLike = CommentLike.create({
+                is_liked,
+                userId: authData.id,
+                commentId: req.params.id,
+              });
+              //save commentLike in database
+              try {
+                await commentLike.save();
+                res.sendStatus(201);
+                return next;
+              } catch (err) {
+                if (err) {
+                  res.json(err);
+                  return err;
+                }
+              }
+            })();
+          }
+        }
+      }
+    );
+  },
+];
+
+export const commentLikeDelete = [
+  verifyToken,
+  (req: any, res: Response, next: NextFunction) => {
+    jwt.verify(
+      req.token,
+      process.env.JWT_SECRET!,
+      (err: any, authData: any) => {
+        if (err) {
+          res.sendStatus(403);
+        } else {
+          try {
+            (async () => {
+              const commentLike = await getRepository(CommentLike)
+                .createQueryBuilder()
+                .delete()
+                .where("id = :id", { id: req.params.id })
+                .execute();
+              res.sendStatus(200);
+            })();
+          } catch (error) {
+            console.log(error);
+            res.sendStatus(400);
+          }
+        }
+      }
+    );
+  },
+];
