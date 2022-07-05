be9907dfea94ebe68c9e49d6070256a8f59e141c
Andrew R <abstractdev0@gmail.com>
2022-07-05 11:41:50 -0400

    add cors

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { DataSource } from "typeorm";
 import express from "express";
+import cors from "cors"
 import dotenv from "dotenv";
 import indexRouter from "./routes/index";
 import usersRouter from "./routes/user";
@@ -37,6 +38,7 @@ AppDataSource.initialize()
 
 try {
   //express middleware
+  app.use(cors());
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
   app.use("/", indexRouter);
